3d4975ad7d36a0f564be3dde2613879ad9bfefc2
static-pages <yxc>
2017-10-24 13:38:05 +0800

    sad

--- app/frontend/models/FileModel.php
+++ app/frontend/models/FileModel.php
@@ -2,20 +2,31 @@
 
 namespace Marser\App\Frontend\Models;
 
-class FileModel extends BaseModel{
+class FileModel extends BaseModel
+{
 
     const TABLE_NAME = 'file';
 
     public $id;
+
     public $file_name;
+
     public $file_path;
+
     public $file_ext;
+
     public $file_size;
+
     public $type;
+
     public $upload_ip;
+
     public $status;
+
     public $create_at;
+
     public $update_at;
+
     public $file_shal;
 
 
